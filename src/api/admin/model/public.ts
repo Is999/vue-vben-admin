8b9eb1f3da15da7d5cf75a818454205170222ab8
billy1 <>
2024-01-30 09:14:47 +0800

    升级到 2.10.1

--- src/api/admin/model/public.ts
+++
@@ -1,6 +0,0 @@
-// 下拉框
-export interface SelectLists {
-  label: string;
-  key: number | string;
-  value: number | string;
-}
--- src/api/sys/model/captcha.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @description: Login interface parameters
- */
-export interface captchaApiResultModel {
-  sensitive: boolean;
-  key: string;
-  img: string;
-}
