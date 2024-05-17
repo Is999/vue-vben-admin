93d3d16b1f537e4ace2054eafefdea826f3575fa
billy1 <>
2024-05-17 17:15:31 +0800

    fix(): 升级到2.11.4

--- src/views/admin/account/setting/SecureSetting.vue
+++ src/views/admin/account/setting/SecureSetting.vue
@@ -8,7 +8,7 @@
               <Icon class="icon" :icon="item.icon" :color="item.color" />
             </template>
             <template #title>
-              <span>{{ item.title }}</span>
+              <span v-html="item.title"></span>
               <div class="extra" v-if="item.extra" @click="item.click">
                 {{ item.extra }}
               </div>
@@ -48,7 +48,9 @@
   const user = userStore.getUserInfo;
   const [registerDrawer, { openDrawer }] = useDrawer();
   const [registerDrawer1, { openDrawer: openDrawer1 }] = useDrawer();
-  const title3 = ref(`校验 MFA(${parseInt(user.mfa_status) === 1 ? '已启用' : '未启用'})`);
+  const title3 = ref(
+    `校验 MFA(${parseInt(user.mfa_status) === 1 ? '<span style="color: green">已启用</span>' : '<span style="color: red">未启用</span>'})`,
+  );
   const extra3 = ref(parseInt(user.mfa_status) === 1 ? '关闭' : '启用');
   // 分页
   const pagination = {
@@ -67,7 +69,7 @@
     },
     {
       id: '2',
-      title: `MAF 设备(${user.exist_mfa ? '已绑定' : '未绑定'})`,
+      title: `MAF 设备(${user.exist_mfa ? '<span style="color: green">已绑定</span>' : '<span style="color: red">未绑定</span>'})`,
       description:
         'MFA设备(基于时间的动态密码并可用于多重身份验证的应用程序)：如 Google Authenticator、Microsoft Authenticator ......',
       extra: user.exist_mfa ? '修改' : '绑定',
@@ -131,7 +133,7 @@
       .then((res) => {
         notify(res, true);
         user.mfa_status = !mfa_status ? 1 : 0;
-        title3.value = `校验 MFA(${parseInt(user.mfa_status) === 1 ? '已启用' : '未启用'})`;
+        title3.value = `校验 MFA(${parseInt(user.mfa_status) === 1 ? '<span style="color: green">已启用</span>' : '<span style="color: red">未启用</span>'})`;
         extra3.value = parseInt(user.mfa_status) === 1 ? '关闭' : '启用';
       })
       .catch((e) => {
